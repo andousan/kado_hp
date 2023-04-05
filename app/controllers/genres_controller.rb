368a8d6ace59637f17a8c6fd8c31027de5449505
andousan <dlm314@outlook.jp>
2023-04-05 23:12:43 +0900

    create genres#edit

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -21,9 +21,16 @@ class GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to genre_path(@genre.id)
+     else
+      render :edit
+     end
   end
 
   def destroy
