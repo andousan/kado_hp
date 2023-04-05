6b27c319e6c6f3ec70a4b8d7dc3abe929ddcc9f9
andousan <dlm314@outlook.jp>
2023-04-05 22:47:55 +0900

    create genres#(new,crearte)

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,8 +1,15 @@
 class GenresController < ApplicationController
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genre_path(@genre)
+    else
+      render :new
+    end
   end
 
   def index
@@ -18,5 +25,10 @@ class GenresController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
