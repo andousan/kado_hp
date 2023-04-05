68d989abe2860bfa44e08cd9444242ec12afabe4
andousan <dlm314@outlook.jp>
2023-04-05 19:55:04 +0900

    create admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   root to: "homes#top"
   get '/about' => "homes#about"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
