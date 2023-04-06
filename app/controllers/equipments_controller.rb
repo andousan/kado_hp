b34ebf1a9352801a8fddfaa70278cbfa2f728044
andousan <dlm314@outlook.jp>
2023-04-06 12:22:17 +0900

    create equipments#index,new,create

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,8 +1,16 @@
 class EquipmentsController < ApplicationController
   def index
+    @equipments = Equipment.all
+    @equipment = Equipment.new
   end
 
   def create
+    @equipment = Equipment.new(equipment_params)
+    if @equipment.save
+      redirect_to equipments_path
+    else
+      render :index
+    end
   end
 
   def edit
@@ -13,5 +21,9 @@ class EquipmentsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+  def equipment_params
+    params.require(:equipment).permit(:name, :detail)
+  end
 end
